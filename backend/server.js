13aca151f5944b6a7ab08450beef5f8dbaf857c2
Gautham P <gauthampdn@gmail.com>
2025-03-30 15:42:47 -0700

    created  docSet routes, controller and renamed model

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 })
 // routes
 app.use('/api/auth', authRoutes);
-
+app.use('/api/notes', noteRoutes);
 // basic route for testing
 app.get('/', (req, res) => {
   res.json({ message: 'API is running' });
