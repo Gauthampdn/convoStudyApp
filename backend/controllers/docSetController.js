76824e551891e065160b87225a714c5ced2ec722
Daniel Gebran <danielgebran92@gmail.com>
2025-04-03 18:50:31 -0600

    Created 'Get 1' API function

--- backend/controllers/docSetController.js
+++ backend/controllers/docSetController.js
@@ -1,1 +1,17 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
+const documentSet = require("../models/docSetModel");
+
+// GET: Fetch a single document set
+const getDocumentSet = async (req, res) => {
+  const { id } = req.params;
+  const { title, files, tags, description, stats } = req.body;
+
+  const docSet = await documentSet.findById(id);
+
+  if (!docSet) {
+    return res.status(404).json({ message: "Document Set not found" });
+  }
+
+  res.status(200).json(docSet);
+};
