3568f9e0af1da6a736a96414473128d059b80c9d
Eric Nguyen <eriicnguyen1@gmail.com>
2025-04-06 16:01:59 -0700

    added Outfit font

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,7 +1,21 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/AuthContext";
+import {
+  useFonts,
+  Outfit_400Regular,
+  Outfit_500Medium,
+  Outfit_600SemiBold,
+  Outfit_700Bold,
+} from "@expo-google-fonts/outfit";
 
 export default function RootLayout() {
+  const [loadedFonts] = useFonts({
+    Outfit_400Regular,
+    Outfit_500Medium,
+    Outfit_600SemiBold,
+    Outfit_700Bold,
+  });
+
   return (
     <AuthProvider>
       <Stack />
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit400 : ["Outfit_400Regular"],
+        outfit500 : ["Outfit_500Medium"],
+        outfit600 : ["Outfit_600SemiBold"],
+        outfit700 : ["Outfit_700Bold"],
+      }
+    },
   },
   plugins: [],
 }
