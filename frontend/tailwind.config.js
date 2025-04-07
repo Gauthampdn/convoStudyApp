6f88dc527d952e13780effa664ab601c1ad5f635
Eric Nguyen <eriicnguyen1@gmail.com>
2025-04-07 00:25:29 -0700

    added more outfit fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        outfit100 : ["Outfit_100Thin"],
+        outfit200 : ["Outfit_200ExtraLight"],
+        outfit300 : ["Outfit_300Light"],
         outfit400 : ["Outfit_400Regular"],
         outfit500 : ["Outfit_500Medium"],
         outfit600 : ["Outfit_600SemiBold"],
         outfit700 : ["Outfit_700Bold"],
+        outfit800 : ["Outfit_800BExtraBold"],
+        outfit900 : ["Outfit_900Black"],
       }
     },
   },
