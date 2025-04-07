6ed5c479b63c83db369f767209b89d9f725176a8
Eric Nguyen <eriicnguyen1@gmail.com>
2025-04-07 00:22:22 -0700

    updated look

--- frontend/app/pages/UploadFiles.tsx
+++ frontend/app/pages/UploadFiles.tsx
@@ -1,114 +1,140 @@
-import React, { useState } from "react"
-import { View, Text, Pressable, Image, FlatList } from "react-native"
-import * as DocumentPicker from "expo-document-picker"
+import { useLayoutEffect, useState } from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+import { useNavigation } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-export default function DocumentUpload(){
-    const [files, setFiles] = useState<{ files: DocumentPicker.DocumentPickerAsset[]; studyDeckNumber: number }[]>([]);
-    let [studyDeckNumber, setStudyDeckNumber] = useState(1)
+export default function DocumentUpload() {
+  const [files, setFiles] = useState<
+    { files: DocumentPicker.DocumentPickerAsset[] }[]
+  >([]);
+  const navigation = useNavigation();
 
-    const selectDocuments = async () => {
-        try{
-            const document = await DocumentPicker.getDocumentAsync({
-            type: [
-                "application/pdf",  // .pdf      
-                "text/plain",       // .txt      
-                "application/vnd.openxmlformats-officedocument.presentationml.presentation", // .pptx
-                "text/markdown",    //. md
-                "audio/mpeg",       // .mp3          
-            ],
-            multiple: true,
-        });
+  // header
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Upload",
+      headerTitleStyle: {
+        fontFamily: "Outfit_600SemiBold",
+        fontSize: 20,
+        color: "#1F1F23",
+      },
+      headerStyle: {
+        backgroundColor: "#F1F3F6",
+      },
+      headerLeft: () => (
+        <Pressable onPress={() => navigation.goBack()}>
+          <Entypo name="chevron-thin-left" size={24} color="#000000" />
+        </Pressable>
+      ),
+      headerLeftContainerStyle: {
+        padding: 100,
+      },
+    });
+  });
 
-        if (!document.canceled && document.assets) {
-            // sorted alphabetically
-            const sortedAssets = [...document.assets].sort((a, b) => a.name.localeCompare(b.name))
+  const selectDocuments = async () => {
+    try {
+      const document = await DocumentPicker.getDocumentAsync({
+        type: [
+          "application/pdf", // .pdf
+        ],
+        multiple: true,
+      });
 
-            const newStudyDeck = {
-                files: sortedAssets,
-                studyDeckNumber,
-            }
-
-            // adds the new study deck on top of the previous ones
-            setFiles((prevFiles) => [newStudyDeck, ...prevFiles])
-            setStudyDeckNumber((prevStudyDeckNumber) => prevStudyDeckNumber + 1)
+      if (!document.canceled && document.assets) {
+        if (document.assets.length > 5) {
+          alert("Only 5 files could be uploaded!");
+          return;
         }
-        } catch (error){
-            console.error(error);
-        }
-        console.log(files)
-        return files
+        // sorted alphabetically
+        const sortedAssets = [...document.assets].sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+
+        const newStudyDeck = {
+          files: sortedAssets,
+        };
+
+        // adds the new study deck on top of the previous ones
+        setFiles((prevFiles) => [newStudyDeck, ...prevFiles]);
+      }
+    } catch (error) {
+      console.error(error);
     }
+    console.log(files);
+    return files;
+  };
 
-    return(
-        <View className="flex-1 bg-white">
-            <Text className="text-[24px] leading-[1.3] font-bold text-center color-[#1F1F23] pt-10">
-                Upload Your Study Materials
+  return (
+    <View className="flex-1 bg-[#F1F3F6]">
+      <Text className="text-[16px] leading-[1.50] font-outfit600 color-[#1F1F23] mt-10 ml-[16]">
+        Media Upload
+      </Text>
+      <Text className="text-[15px] leading-md font-outfit300 color-[#6D6D6D] mt-[9] m-[16]">
+        Add your documents here, and you can upload up to 5 files max
+      </Text>
+
+      <View className="items-center">
+        {/* upload box */}
+        <Pressable
+          className="border-[1px] border-dashed border-[#1849D6] h-[194px] w-[361px] bg-white rounded-[8px] items-center justify-center"
+          onPress={selectDocuments}
+        >
+          <MaterialCommunityIcons
+            className=""
+            name="folder-upload"
+            size={50}
+            color="#2879FF"
+          />
+          <Text className="text-[15px] font-outfit300 color-[#0B0B0B] leading-md m-2">
+            Drag your file(s) to start uploading
+          </Text>
+          <Text className="text-[15px] font-outfit300 color-[#5A5E6B] leading-md mb-2">
+            OR
+          </Text>
+
+          {/* browse button */}
+          <Pressable
+            className="w-[102px] h-[36px] bg-[#FFFFFF] border-[2px] border-[#2879FF] rounded-[12px] items-center justify-center"
+            onPress={selectDocuments}
+          >
+            <Text className="text-[15px] font-outfit500 leading-[1.3] color-[#2879FF]">
+              Browse
             </Text>
-            <Text className="text-[16px] leading-[1.5] text-center color-[#1f1f23] pt-3 p-12 pb-8">
-                Add PDFs, Docs, or Notes to generate AI-powered study guides 
-            </Text>
+          </Pressable>
+        </Pressable>
+      </View>
 
-            <View className="items-center">
-                {/* upload box */}
-                <Pressable 
-                    className="border-[2px] border-dashed border-[#7C8B9D] h-[136px] w-[322px] rounded-[10px] items-center justify-center"
-                    onPress={selectDocuments}
-                >
-                    <Image 
-                        source={require("../../assets/images/UploadIcon.png")}
-                        resizeMode="contain"
-                    />
-                    <Text className="text-[14px] font-semibold color-[#7C8B9D] leading-[1.5]">
-                        Select or drag documents
-                        </Text>
-                    <Text className="text-[12px] color-[#a3a9b3] leading-[1.3]">
-                        Supported file types: .pdf, .txt, .pptx, .MD, .mp3
-                    </Text>
-                </Pressable>
-                
-                {/* upload button */}
-                <Pressable 
-                    className="w-[113px] h-[40px] bg-[#2879FF] rounded-[12] items-center justify-center m-8"
-                    onPress={selectDocuments}
-                >
-                    <Text className="text-[16px] font-semibold leading-[1.3] color-white">
-                        Select
-                    </Text>
-                </Pressable>
-            </View>
-
-            <Text className="text-[20px] font-semibold leading-[1.3] color-[#1f1f23] pl-[33px]">
-                Uploaded Study Materials
-            </Text>
-
-            {files.length === 0 ? (
-                <View className="flex-1 justify-center items-center">
-                    <Text className="text-[16px] font-semibold color-[#a3a9b3] leading-[1.3]">
-                        No materials uploaded yet
-                    </Text>
-                </View>
-            ) : (
-                <FlatList
-                    data={ files }
-                    renderItem={({ item }) => (
-                        <View className="bg-gray-100 p-3 rounded-lg mb-4">
-                        <Text className="text-[16px] font-semibold pl-[30px] pb-2">
-                            Study Deck {item.studyDeckNumber}
-                        </Text>
-                        {item.files.map((file, index) => (
-                            <View
-                                key={index}
-                                className="bg-white p-2 mb-1 rounded-md"
-                            >
-                                <Text className="text-[14px] text-black pl-[30px]">
-                                    {file.name}
-                                </Text>
-                            </View>
-                        ))}
-                        </View>
-                    )}
-                />
-            )}
+      {files.length === 0 ? (
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-[16px] font-outfit500 color-[#a3a9b3] leading-[1.3]">
+            No documents uploaded yet
+          </Text>
         </View>
-    );
+      ) : (
+        <FlatList
+          data={files}
+          renderItem={({ item }) => (
+            <View className="">
+              {item.files.map((file, index) => (
+                <View key={index} className="bg-white p-2 mb-1 rounded-md">
+                  <Text className="text-[14px] text-black pl-[30px]">
+                    {file.name}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          )}
+        />
+      )}
+    </View>
+  );
 }
