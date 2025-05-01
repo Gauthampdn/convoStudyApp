1fa433a34dd73b1bb4f66c1c646ccf7a51da7d14
Eric Nguyen <eriicnguyen1@gmail.com>
2025-04-30 17:07:44 -0700

    Create UploadFilesButton.tsx

---
+++ frontend/app/components/UploadFilesButton.tsx
@@ -0,0 +1,20 @@
+// UploadFilesButton.tsx
+import { Text, TouchableOpacity } from "react-native";
+
+type UploadFilesButtonProps = {
+  text: string;
+  onPress: () => void | Promise<void>;
+};
+
+export default function UploadFilesButton({ text, onPress }: UploadFilesButtonProps) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      className="w-[102px] h-[36px] bg-[#FFFFFF] border-[2px] border-[#2879FF] rounded-[12px] items-center justify-center"
+    >
+      <Text className="text-[15px] font-outfit500 leading-[1.3] text-[#2879FF]">
+        {text}
+      </Text>
+    </TouchableOpacity>
+  );
+}
